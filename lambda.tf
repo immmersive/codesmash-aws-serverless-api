600cdaa2335b363b95c341e796769f3a7ee94553
Immersive <105320165+immmersive@users.noreply.github.com>
2023-09-04 02:17:52 +0900

    Create lambda.tf

---
+++ lambda.tf
@@ -0,0 +1,20 @@
+resource "aws_lambda_function" "lambda" { 
+    filename            = "lambda_code.zip" 
+    function_name       = "${var.app_name}_${terraform.workspace}"
+    role                = aws_iam_role.role.arn
+    handler             = "index.handler"
+    memory_size         = "128"
+    timeout             = "60"
+ 
+    source_code_hash    = "${base64sha256("lambda_code.zip")}"
+
+    runtime             = "nodejs18.x"
+  
+    environment {
+        variables = {
+            region = var.region            
+            database = aws_dynamodb_table.dynamodb.id
+            stage = terraform.workspace
+        }
+    } 
+}
