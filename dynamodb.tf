d91f83c3d4ef8881414a85befa79b611ef917899
Immersive <105320165+immmersive@users.noreply.github.com>
2024-04-02 00:59:51 +0900

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -25,45 +25,6 @@ resource "aws_dynamodb_table" "dynamodb" {
         type = "S"
     } 
 
-    attribute {
-        name = "type1"
-        type = "S"
-    } 
-
-    attribute {
-        name = "type2"
-        type = "S"
-    } 
-
-    attribute {
-        name = "type3"
-        type = "S"
-    } 
-
-    attribute {
-        name = "type4"
-        type = "S"
-    } 
-
-    attribute {
-        name = "type5"
-        type = "S"
-    } 
-
-    global_secondary_index {
-        name            = "type-pk-index"
-        hash_key        = "type"
-        range_key       = "pk"
-        projection_type = "ALL"
-    }
-
-    global_secondary_index {
-        name            = "type-sk-index"
-        hash_key        = "type"
-        range_key       = "sk"
-        projection_type = "ALL"
-    }
-
     global_secondary_index {
         name            = "pk-type-index"
         hash_key        = "pk"
@@ -81,36 +42,6 @@ resource "aws_dynamodb_table" "dynamodb" {
     global_secondary_index {
         name            = "type-index"
         hash_key        = "type"
-        projection_type = "ALL"
-    }
-
-    global_secondary_index {
-        name            = "type1-index"
-        hash_key        = "type1"
-        projection_type = "ALL"
-    }
-
-    global_secondary_index {
-        name            = "type2-index"
-        hash_key        = "type2"
-        projection_type = "ALL"
-    }
-
-    global_secondary_index {
-        name            = "type3-index"
-        hash_key        = "type3"
-        projection_type = "ALL"
-    }
-
-    global_secondary_index {
-        name            = "type4-index"
-        hash_key        = "type4"
-        projection_type = "ALL"
-    }
-
-    global_secondary_index {
-        name            = "type5-index"
-        hash_key        = "type5"
         projection_type = "ALL"
     }
 }
