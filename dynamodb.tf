8e2bfb2986671bd638582ed64ef237e560c54d13
Immersive <105320165+immmersive@users.noreply.github.com>
2024-05-06 01:43:00 +0900

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -6,6 +6,10 @@ resource "aws_dynamodb_table" "dynamodb" {
     stream_view_type = "NEW_AND_OLD_IMAGES"
     billing_mode     = "PAY_PER_REQUEST"
 
+    lifecycle {
+        prevent_destroy = true
+    }
+
     point_in_time_recovery {
         enabled = true
     }
