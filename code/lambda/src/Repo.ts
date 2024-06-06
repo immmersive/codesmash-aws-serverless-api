739d1db79c1d4616e72dd0bede3a37816eca5598
Immersive <105320165+immmersive@users.noreply.github.com>
2024-06-06 23:48:34 +0900

    Update Repo.ts

--- code/lambda/src/Repo.ts
+++ code/lambda/src/Repo.ts
@@ -1,19 +1,70 @@
-import { NewType } from "./actions/NewType";
-import { GetType } from "./actions/GetType";
-import { UpdateType } from "./actions/UpdateType";
-import { DeleteType } from "./actions/DeleteType";
-import { QueryType } from "./actions/QueryType";
-  
+import { ApiDefinition } from "./ApiDefinition"
+import { Route } from "./Route"
+import { ContainsAction } from "./functions/ContainsAction"
+import { DeleteItemAction } from "./functions/DeleteItemAction"
+import { DoesntContainAction } from "./functions/DoesntContainAction"
+import { EqualsAction } from "./functions/EqualsAction"
+import { FetchAction } from "./functions/FetchAction"
+import { GetItemAction } from "./functions/GetItemAction"
+import { GreaterThanAction } from "./functions/GreaterThanAction"
+import { GreaterThanOrEqualAction } from "./functions/GreaterThanOrEqualAction"
+import { InsertItemAction } from "./functions/InsertItemAction"
+import { LessThanAction } from "./functions/LessThanAction"
+import { LessThanOrEqualAction } from "./functions/LessThanOrEqualAction"
+import { NotEqualAction } from "./functions/NotEqualAction"
+import { QueryItemsAction } from "./functions/QueryItemsAction"
+import { SetValueAction } from "./functions/SetValueAction"
+import { UpdateItemAction } from "./functions/UpdateItemAction"
+import { ReturnAction } from "./functions/ReturnAction"
+ 
 export class Repo
-{ 
-    getActions()
+{  
+    apiDefinition: ApiDefinition
+
+    constructor()
     { 
+        this.apiDefinition = new ApiDefinition();
+    }
+
+    getRoutes(): 
+    { 
+        invokeRoute(queryParameters: any, headers: any, path: any, body: any): Promise<any>
+        isMatching(path: string, httpMethod: string): boolean
+    }[]
+    {  
+        return this.apiDefinition.definitions.map(x => 
+            new Route(
+                x.route, 
+                x.method, 
+                x.funcInvocations
+                    .filter(f => f.skip === false)
+                    .map(f => this.functionSelector(f.funcId))));
+    }
+
+    functionSelector(funcId: string): 
+    {
+        id: string,
+        run(data: any, source: any, other: any): Promise<boolean>
+    }
+    { 
+        
         return [
-            new NewType(),
-            new GetType(),
-            new UpdateType(),
-            new DeleteType(),
-            new QueryType()
-        ]
+            new ContainsAction(),
+            new DeleteItemAction(),
+            new DoesntContainAction(),
+            new EqualsAction(),
+            new FetchAction(),
+            new GetItemAction(),
+            new GreaterThanAction(),
+            new GreaterThanOrEqualAction(),
+            new InsertItemAction(),
+            new LessThanAction(),
+            new LessThanOrEqualAction(),
+            new NotEqualAction(),
+            new QueryItemsAction(),
+            new SetValueAction(),
+            new UpdateItemAction(),
+            new ReturnAction()
+        ].filter(x => x.id === funcId)[0];
     }
 }
