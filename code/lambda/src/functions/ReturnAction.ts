b5d15cb27fda50901d55e4e5b1cac93823b0e3a0
Immersive <105320165+immmersive@users.noreply.github.com>
2024-06-06 23:59:07 +0900

    Update ReturnAction.ts

--- code/lambda/src/functions/ReturnAction.ts
+++ code/lambda/src/functions/ReturnAction.ts
@@ -11,30 +11,17 @@ export class ReturnAction
         var help = new HelpApi();
         var value1 = source.value1;        
         var toSet = null;
-
-        console.log('in return');
-        console.log(value1);
-        console.log('in return');
  
         if(help.getObjectValue(data, value1) || help.getObjectValue(data, value1) === 0)
-        {
-            console.log('help.getObjectValue');
-            toSet = help.getObjectValue(data, value1);
-            console.log('help.getObjectValue');
-            console.log(toSet);
+        { 
+            toSet = help.getObjectValue(data, value1); 
         }
         else 
         { 
             return false;
         }
-
-        console.log('exiting return');
-        console.log(toSet);
-
+ 
         help.setObjectValue(data, 'return', value1);
-
-        console.log('after set object');
-        console.log(data);
  
         return true;
     }
