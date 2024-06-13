93c496329d65dc869b5ecede80ce8d1a60f26d62
Immersive <105320165+immmersive@users.noreply.github.com>
2024-06-13 21:03:14 +0900

    Update SetValueAction.ts

--- code/lambda/src/functions/SetValueAction.ts
+++ code/lambda/src/functions/SetValueAction.ts
@@ -10,63 +10,12 @@ export class SetValueAction
     {
         var help = new HelpApi();
         var value1 = source.value1;
-        var value2 = source.value2;        
-        var toSet = null;
+        var value2 = source.value2;     
  
-        if((value1.startsWith('"') && value1.endsWith('"')) || (value1.startsWith("'") && value1.endsWith("'")))
-        {  
-            toSet = value1.slice(1, -1); 
-        }
-        else if(!isNaN(value1))
-        {  
-            toSet = Number(value1);
-        }
-        else if(value1.startsWith('[') && value1.endsWith(']'))
-        {  
-            try 
-            {
-                toSet = JSON.parse("[" + value1.slice(1, -1) + "]") as any[];
-            } 
-            catch (error) 
-            {
-                toSet = undefined;
-            }
-        }
-        else if(value1.startsWith('{') && value1.endsWith('}'))
-        {  
-            try 
-            {
-                toSet = new Function('data', 'return ' + value1)(data);
-            } 
-            catch (error)  
-            {
-                toSet = undefined;
-            }
-        }
-        else if(value1.startsWith('@'))
-        { 
-            toSet = new Function('data', 'return ' + value1.substring(1))(data);
-        }
-        else if(value1.includes('(') && value1.includes(')'))
-        { 
-            try 
-            {
-                toSet = new Function('data', 'return data.' + value1)(data);
-            } 
-            catch (error) 
-            {
-                console.log(error);
-            }
-            
-        }        
-        else if(help.getObjectValue(data, value1) || help.getObjectValue(data, value1) === 0)
-        { 
-            toSet = help.getObjectValue(data, value1);
-        }
-        else 
-        {  
-            return false;
-        }
+        help.setObjectValue(
+            data, 
+            value2, 
+            new Function('data', 'return ' + help.prefixVars(data, value1))(data));
  
         help.setObjectValue(data, value2, toSet);
  
