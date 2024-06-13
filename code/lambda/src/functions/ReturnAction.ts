0abb4ae4a93f22fcaf7624f2d2425d2bf4076169
Immersive <105320165+immmersive@users.noreply.github.com>
2024-06-13 21:02:25 +0900

    Update ReturnAction.ts

--- code/lambda/src/functions/ReturnAction.ts
+++ code/lambda/src/functions/ReturnAction.ts
@@ -9,18 +9,8 @@ export class ReturnAction
         source: { value1: any }) : Promise<boolean>
     { 
         var help = new HelpApi();
-        var value1 = source.value1;        
-        var toSet = null;
- 
-        if(help.getObjectValue(data, value1) || help.getObjectValue(data, value1) === 0)
-        { 
-            toSet = help.getObjectValue(data, value1); 
-        }
-        else 
-        { 
-            return false;
-        }
- 
+        var value1 = source.value1;       
+  
         help.setObjectValue(data, 'return', value1);
  
         return true;
