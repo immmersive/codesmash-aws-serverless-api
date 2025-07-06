28d611f7be69693547245d9a36e3f6bedfe40319
Immersive <105320165+immmersive@users.noreply.github.com>
2025-07-06 22:13:59 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,12 @@
-provider "aws" {
-  region = var.region
+provider "aws" {  
+    region     = var.region
 }
-
+  
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
-    }
-  }
-
-  backend "s3" {
-    encrypt = true
-    bucket  = var.bucket
-    region  = var.region
-    key     = var.key
-  }
+ backend "s3" {
+ encrypt = true
+ bucket = var.bucket
+ region = var.region
+ key = var.key
+ }
 }
